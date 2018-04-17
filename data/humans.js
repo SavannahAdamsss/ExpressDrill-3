9955547ab29fb8a522650c574670b70e7163fb1b
SavannahAdamsss <savannah.adams0@gmail.com>
2018-04-17 13:55:48 -0600

    bug

---
+++ data/humans.js
@@ -0,0 +1,26 @@
+module.exports = [{
+    id: 1,
+    firstName: "Van",
+    lastName: "Kenny",
+    planet: "Mars"
+},{
+    id: 2,
+    firstName: "Builder",
+    lastName: "Bill",
+    planet: "Venus"
+},{
+    id: 3,
+    firstName: "Jack",
+    lastName: "Two Horses",
+    planet: "Saturn"
+},{
+    id: 4,
+    firstName: "Snow",
+    lastName: "Bird",
+    planet: "Mercury"
+},{
+    id: 5,
+    firstName: "Bus",
+    lastName: "Kenny",
+    planet: "Uranus"
+}];
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const index = express();
+const data = require("./data/humans");
+let PORT = process.env.PORT || 3000;
+
+function findById(data, id){
+    for (let i = 0; i < data.length; i++){
+        if (data[i].id == id){
+            return data[i];
+        }
+    }
+    return null;
+}
+
+index.use(cors());
+
+index.get("/", function (request, response) {
+    response.json({data});
+});
+
+index.get("/:id", function (request, response) {
+    var record = findById(data, request.params.id);
+    if (!record){
+        response.status(404).json({
+            error: {
+                message: "No record found!"
+            }
+        });
+    } else {
+        response.json({data: record});
+    }
+});
+
+index.listen(PORT);
